147b51785c28b5b676f8aa9b8be93f3c61576707
Apprentice <apprentice@dbc-chicago-021.local>
2014-03-14 17:07:52 -0500

    backing up to swithc to master at five pm

--- sinatra_skeleton/app/models/card.rb
+++ sinatra_skeleton/app/models/card.rb
@@ -1,12 +1,8 @@
 class Card < ActiveRecord::Base
   # Remember to create a migration!
-<<<<<<< HEAD
-  validates :question, uniqueness: true, presence: true
-  validates :name, presence: true
-=======
-  # validates :qsad'pokfasddlas'lkfj'estion, uniqueness: true
+  # validates :question, uniqueness: true
   # validates :answer, presence: true
->>>>>>> 8ac7e09c92582670405ff75fdf08b997f74070ba
+
 
   belongs_to :deck
 
