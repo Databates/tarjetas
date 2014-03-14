123eda501f4545d9368d4292a3bf68a09ca1eac6
Apprentice <apprentice@dbc-chicago-021.local>
2014-03-14 14:58:13 -0500

    three pm version

---
+++ sinatra_skeleton/app/models/card.rb
@@ -0,0 +1,8 @@
+class Card < ActiveRecord::Base
+  # Remember to create a migration!
+  # validates :qsad'pokfasddlas'lkfj'estion, uniqueness: true
+  # validates :answer, presence: true
+
+  belongs_to :deck
+
+end
