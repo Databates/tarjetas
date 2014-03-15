71545c19a15b2535e8f08bb80f893c4b16d9e642
Ian Bates <38bates@gmail.com>
2014-03-14 19:23:34 -0500

    backed up before commit seven thirty

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -1,14 +1,44 @@
 enable :sessions
-exi
 # http://www.sinatrarb.com/faq.html#sessions
 get '/' do
   # @decks = Deck.all
   erb :index
 end
 
-post "/round" do
-  @deck_id = Deck.find_by_name(params[:name]).id
-  @cards = Card.where(deck_id: @deck_id)
-  return "cheese"
-  erb :round
-end
+# jquery routing ---- user login
+
+
+
+#
+
+#send card to module
+  get '/send_card' do
+    @cards.shuffle!
+    card = @cards.pop
+#    erb :send_card , locals: { card:}
+    return "swiss"
+  end
+
+
+# post--- r
+#post '/user_guess' do
+
+
+#erb: display_correct
+#end
+
+
+
+#increment score
+# methods in card class to evaluate our 50 questoinsw
+
+
+#def evaluate_correct_response
+
+# card = Card.where(card_id: )
+
+
+#end
+
+
+
